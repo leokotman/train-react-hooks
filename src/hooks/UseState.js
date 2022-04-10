13963200afffe5543e205d4a21e4b5167a3ba9e8
levkotman <leoktmn@yandex.ru>
2022-04-10 20:29:51 +0200

    useState task done

---
+++ src/hooks/UseState.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const UseState = () => {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const changeInputValue = (event) => {
+    let newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
+  const addItem = () => {
+    const newItem = {
+      id: Math.floor(Math.random() * 1000),
+      text: inputValue,
+    };
+    setItems([...items, newItem]);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <h3>UseState</h3>
+      <input
+        placeholder="Write a new item in the list"
+        onChange={changeInputValue}
+        value={inputValue}
+        onKeyDown={(ev) => {
+          if (ev.key === "Enter") {
+            addItem();
+          }
+        }}
+      />
+      <button onClick={addItem}>Add item</button>
+      <div>
+        {items.map((item) => (
+          <p key={item.id}>
+            {item.id} {item.text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default UseState;
